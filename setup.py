e075718a7c54e287467b41482bf1906ef432f909
Moritz Kassner <mkassner@ubuntu.(none)>
2014-09-27 10:40:11 -0700

    adding support for 32bit os install

--- setup.py
+++ setup.py
@@ -4,7 +4,13 @@ import numpy
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
+import glob
 
+try:
+    #check for tubo jpeg offical lib and select appropriate lib32/lib64 path.
+    tj_lib = glob.glob('/opt/libjpeg-turbo/lib*')[0]+'/libturbojpeg.a'
+except IndexError:
+   raise Exception("Please install libturbojpeg")
 
 extensions = [
     Extension(  name="v4l2",
@@ -12,7 +18,7 @@ extensions = [
                 include_dirs =  [numpy.get_include()],
                 libraries = ['v4l2','rt'],
                 extra_link_args=[],
-                extra_objects = ['/opt/libjpeg-turbo/lib64/libturbojpeg.a'], #can we use the static lib here? is that legal?
+                extra_objects = [tj_lib],
                 extra_compile_args=[]
             ),
 ]
